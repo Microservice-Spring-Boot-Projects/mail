f4c444a4c2ce566ee1e029fbb27493940f0f947c
celle2006 <marcel.lindhorst@googlemail.com>
2026-05-16 09:27:05 +0200

    java 25 and pipeline

--- src/main/java/ml/dev/mail/MailApplication.java
+++ src/main/java/ml/dev/mail/MailApplication.java
@@ -3,10 +3,9 @@ package ml.dev.mail;
 import ml.dev.mail.service.ConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 @EnableConfigurationProperties(ConfigProperties.class)
 public class MailApplication {
 
--- src/test/java/ml/dev/mail/MailApplicationTests.java
+++ src/test/java/ml/dev/mail/MailApplicationTests.java
@@ -2,8 +2,10 @@ package ml.dev.mail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MailApplicationTests {
 
 	@Test
